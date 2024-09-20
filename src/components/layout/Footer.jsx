01a0a196c2c0c43c62d0c3097504a94446116952
Akdevit <abhishek656550@gmail.com>
2024-09-20 12:14:55 +0530

    add color

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
             <div className='w-full h-auto'>
                 <h1 className='footerh1 xl:text-[150px] text-[70px] text-center'>Start Now. Hire Us! </h1>
             </div>
-            <p className='p-2 text-center'>Thank You for Visiting My Portfolio! <span>&#x2764;</span></p>
+            <p className='p-2 text-center'>Thank You for Visiting My Portfolio! <span className='text-black'>&#x2764;</span></p>
         </>
     )
 }
